743ed384978e9dbf2941ae616bdee7fdb0425b0f
sachikoinverna <sachikoinverna@gmail.com>
2025-02-07 19:49:12 +0100

    requestmodel, responsemodel

---
+++ Models/RequestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCGErcilla.Models
+{
+    internal class RequestModel
+    {
+        // Propiedad que representa el método HTTP de la solicitud (GET, POST, etc.).
+        public string Method { get; set; }
+
+        // Propiedad que almacena la ruta de la solicitud dentro del servidor.
+        public string Route { get; set; }
+
+        // Propiedad que contiene los datos asociados a la solicitud (puede ser cualquier tipo de datos).
+        public object Data { get; set; }
+
+        // Constructor parametrizado de la clase RequestModel.
+        public RequestModel(string method, string route, object data)
+        {
+            Method = method;
+            Route = route;
+            Data = data;
+        }
+
+        public RequestModel()
+        {
+        }
+    }
+}
